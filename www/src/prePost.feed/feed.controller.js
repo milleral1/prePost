0a49bedec3fa4c07dc42f0a92b37fb107a45823c
alon miller <alon2271998@gmail.com>
2014-09-20 12:00:06 +0300

    adding image modal

--- www/src/prePost.feed/feed.controller.js
+++ www/src/prePost.feed/feed.controller.js
@@ -10,8 +10,18 @@
     }).then(function(modal) {
         scope.modal = modal;
     });
-  }
 
+      ionicModal.fromTemplateUrl('src/prePost.feed/image.html', {
+          scope: scope
+      }).then(function(modal) {
+          scope.modal2 = modal;
+      });
+
+      scope.modal2Selected = function (postId) {
+          scope.post = Posts.get(postId);
+          scope.modal2.show();
+      }
+  }
     FeedCtrl.$inject = ['$log', 'Posts', '$scope','$ionicModal'];
 
   angular.module('prePost.feed')
