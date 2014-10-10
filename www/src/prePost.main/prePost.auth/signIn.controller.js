2b6649654bf81f7791310526c5c2e5bb6379a18a
alon miller <alon2271998@gmail.com>
2014-10-10 18:35:38 +0300

    adding more features to sign in controller

--- www/src/prePost.main/prePost.auth/signIn.controller.js
+++ www/src/prePost.main/prePost.auth/signIn.controller.js
@@ -1,15 +1,21 @@
 (function() {
-    function SignInCtrl(state, log, Auth) {
+    function SignInCtrl(state, log, Auth, ionicPopup) {
         var _this = this;
         log.debug('sign in ctrl!!!');
         _this.register = function(email, password) {
             Auth.signIn(email, password).then(function(data) {
                 log.debug('data in controller', data);
 //                state.go('app.feed');
+            }, function(data) {
+                log.error(data);
+                ionicPopup.alert({
+                    title: 'error',
+                    template: data
+                });
             });
         };
     }
-        SignInCtrl.$inject = ['$state', '$log', 'Auth'];
+        SignInCtrl.$inject = ['$state', '$log', 'Auth', '$ionicPopup'];
 
     angular.module('prePost.auth')
         .controller('SignInCtrl', SignInCtrl)
